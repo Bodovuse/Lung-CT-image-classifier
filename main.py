382522bf4e7aa56b9d6ec855badc38a32f67452b
bodovuse <bodovuse@gmail.com>
2026-08-30 22:02:06 +0100

    initial setup

---
+++ main.py
@@ -0,0 +1,9 @@
+# Import necessary libraries
+import sys
+import warnings
+import tensorflow
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+import matplotlib.pyplot as plt
